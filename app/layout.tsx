5721f28def977d9484d3283e1a472081bcb57c00
danyakmallun9999 <danyakmallun9999@gmail.com>
2025-12-14 22:47:56 +0700

    default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -145,7 +145,7 @@ export default function RootLayout({
           enableSystem={true}
           attribute="class"
           storageKey="theme"
-          defaultTheme="system"
+          defaultTheme="light"
         >
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
             <ScrollProgressBar />
